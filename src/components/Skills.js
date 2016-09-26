a3929435b5cd30c4d43ab9297a8efee486e5e6cc
urrri <yury.go@gmail.com>
2016-09-26 21:59:43 +0300

    edit skill name; prepared for add/delete skill

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -15,21 +15,21 @@ export default class Skills extends Component {
     super(props);
     this.state = {
       editing: false,
-      skills: props.skills
+      skills: _.toPairs(props.skills)
     };
     autoBind(this);
   }
 
   componentWillReceiveProps(props) {
     this.setState({
-      skills: props.skills
+      skills: _.toPairs(props.skills)
     });
   }
 
   toggleEditing() {
     const { editing, skills } = this.state;
     if (editing) {
-      this.props.saveSkills(skills);
+      this.props.saveSkills(_.fromPairs(skills));
     }
     this.props.editingChanged(!this.state.editing);
     this.setState({ editing: !this.state.editing });
@@ -42,38 +42,41 @@ export default class Skills extends Component {
   }
 
   render() {
-    const { editing } = this.state;
-    const { skills } = this.props;
+    const { editing, skills } = this.state;
     let skls;
     if (!editing) {
-      skls = _.reduce(skills, (result, value, key) => {
-        result.push(<div key={key} className="row">
+      skls = _.map(skills, (skill) => {
+        const [key, value] = skill;
+        return (<div key={key} className="row">
           <span className="col-md-2">{key}:</span>
           <span className="col-md-9">{value}</span>
         </div>);
-        return result;
       }, []);
     } else {
-      skls = _.reduce(skills, (result, value, key) => {
-        result.push(<div key={key} className="row">
-          <span className="col-md-2">{key}:</span>
-          <span className="col-md-9">
-            <input
-              type="number"
-              maxLength="2"
-              className="form-control"
-              defaultValue={value}
-              onChange={this.updateSkill.bind(this, key)}
-            />
-          </span>
+      skls = _.map(skills, (skill) => {
+        const [name, value] = skill;
+        return (<div key={name} className="row">
+          <i onClick={() => {}} className="glyphicon glyphicon-remove " />
+          <input
+            type="text"
+            className="form-control"
+            defaultValue={name}
+            onChange={(e) => { skill[0] = e.target.value; }}
+          />
+          <input
+            type="number"
+            maxLength="2"
+            className="form-control"
+            defaultValue={value}
+            onChange={(e) => { skill[1] = e.target.value; }}
+          />
         </div>);
-        return result;
       }, []);
     }
 
     return (
       <div className="container form-inline">
-        <div className="row" >
+        <div className="row">
           <EditToggle onToggleEditing={this.toggleEditing} editing={editing} />
         </div>
         {skls}
